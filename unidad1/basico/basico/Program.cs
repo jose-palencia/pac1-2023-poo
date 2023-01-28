925471310436d53d216e2c368ad12fc4d96ab4d0
jose-palencia <joc.palencia+code@gmail.com>
2023-01-27 19:01:04 -0600

    primer proyecto con tipos de datos.

---
+++ unidad1/basico/basico/Program.cs
@@ -0,0 +1,18 @@
+﻿using basico.tipo_datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tipos01 t01 = new Tipos01();
+            Tipos01 t02 = new Tipos01();
+        }
+    }
+}
---
+++ unidad1/basico/basico/tipo-datos/Tipos01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basico.tipo_datos
+{
+    internal class Tipos01
+    {
+        public Tipos01()
+        {
+            Console.WriteLine("Hola desde Tipos01");
+        }
+    }
+}
